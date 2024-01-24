4562f0bc781ef268a7788b0641b0276c6c0aa239
Exequiel Ceasar Navarrete <dev@exequielnavarrete.me>
2024-01-24 11:17:26 +0800

    setup firebase

--- apps/nx-ng-seventeen-test/src/app/app.config.ts
+++ apps/nx-ng-seventeen-test/src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    importProvidersFrom(
+      provideFirebaseApp(() => {
+        return initializeApp({
+          // TODO add project here
+          apiKey: '',
+          appId: '',
+          projectId: '',
+        });
+      })
+    ),
+    importProvidersFrom(
+      provideRemoteConfig(() => {
+        const remoteConfig = getRemoteConfig();
+
+        return remoteConfig;
+      })
+    ),
+  ],
 };
