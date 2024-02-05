f6ea7c7c4836c1aab4e49002805184ac86ad9801
Exequiel Ceasar Navarrete <dev@exequielnavarrete.me>
2024-02-05 15:25:28 +0800

    add app initializer

--- apps/nx-ng-seventeen-no-firebase/src/app/app.config.ts
+++ apps/nx-ng-seventeen-no-firebase/src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { timer } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [provideClientHydration(), provideRouter(appRoutes), {
+    provide: APP_INITIALIZER,
+    multi: true,
+    useValue: () => timer(300)
+  }],
 };
